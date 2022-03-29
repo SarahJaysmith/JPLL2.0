0e7e404f5850fa0ccc26c81274762d3ec5d48e05
Sarah Jaysmith <sarahjaysmith@hotmail.com>
2022-03-28 23:21:31 -0700

    Initial Commit

---
+++ JPLL2.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JPLL2.0
+//
+//  Created by Sarah Jaysmith on 2022-03-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JPLL2.0/JPLL2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  JPLL2_0App.swift
+//  JPLL2.0
+//
+//  Created by Sarah Jaysmith on 2022-03-28.
+//
+
+import SwiftUI
+
+@main
+struct JPLL2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
